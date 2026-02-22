c50143ba2c2d59ae65df064b4ff901dcb1632452
Lander03xD <22810909+lander03xD@users.noreply.github.com>
2026-02-22 20:49:58 +0100

    refactoring the code and fixing some vibe mistakes

--- src/behaviors/behavior_morse.c
+++ src/behaviors/behavior_morse.c
@@ -12,8 +12,8 @@
 #include <drivers/behavior.h>
 #include <zmk/behavior.h>
 #include <dt-bindings/zmk/keys.h>
-#include <dt-bindings/zmk/hid_usage.h>
 #include <zmk/events/keycode_state_changed.h>
+#include <zephyr/sys/util.h> //for ARG_UNUSED(work);
 
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
@@ -21,13 +21,15 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
 
 #define MAX_SYMBOLS 6 
+#define MORSE_TIME_UNIT DT_INST_PROP(0, time_unit_ms); 
 
 struct morse_state { 
     // morse buffer
     char buffer[MAX_SYMBOLS]; //buffer of collected dots and dashes
     uint8_t count; //amount of dots and dashes
     // timings
-    int64_t last_release_time; 
+    int64_t press_start_time; // to compute dot vs dash duration todo not sure if this is needed
+    int64_t last_event_timestamp; // safe timestamp for emitting events
     //work 
     struct k_work_delayable flush_letter_work; // no clue
     struct k_work_delayable flush_word_work; // no clue   
@@ -41,44 +43,48 @@ struct morse_mapping {
 };
 
 static const struct morse_mapping morse_table[] = {
-    {".-", HID_USAGE_KEY_KEYBOARD_A},
-    {"-...", HID_USAGE_KEY_KEYBOARD_B},
-    {"-.-.", HID_USAGE_KEY_KEYBOARD_C},
-    {"-..", HID_USAGE_KEY_KEYBOARD_D},
-    {".", HID_USAGE_KEY_KEYBOARD_E},
-    {"..-.", HID_USAGE_KEY_KEYBOARD_F},
-    {"--.", HID_USAGE_KEY_KEYBOARD_G},
-    {"....", HID_USAGE_KEY_KEYBOARD_H},
-    {"..", HID_USAGE_KEY_KEYBOARD_I},
-    {".---", HID_USAGE_KEY_KEYBOARD_J},
-    {"-.-", HID_USAGE_KEY_KEYBOARD_K},
-    {".-..", HID_USAGE_KEY_KEYBOARD_L},
-    {"--", HID_USAGE_KEY_KEYBOARD_M},
-    {"-.", HID_USAGE_KEY_KEYBOARD_N},
-    {"---", HID_USAGE_KEY_KEYBOARD_O},
-    {".--.", HID_USAGE_KEY_KEYBOARD_P},
-    {"--.-", HID_USAGE_KEY_KEYBOARD_Q},
-    {".-.", HID_USAGE_KEY_KEYBOARD_R},
-    {"...", HID_USAGE_KEY_KEYBOARD_S},
-    {"-", HID_USAGE_KEY_KEYBOARD_T},
-    {"..-", HID_USAGE_KEY_KEYBOARD_U},
-    {"...-", HID_USAGE_KEY_KEYBOARD_V},
-    {".--", HID_USAGE_KEY_KEYBOARD_W},
-    {"-..-", HID_USAGE_KEY_KEYBOARD_X},
-    {"-.--", HID_USAGE_KEY_KEYBOARD_Y},
-    {"--..", HID_USAGE_KEY_KEYBOARD_Z},
-    {"-----", HID_USAGE_KEY_KEYBOARD_0_AND_RIGHT_PARENTHESIS},
-    {".----", HID_USAGE_KEY_KEYBOARD_1_AND_EXCLAMATION},
-    {"..---", HID_USAGE_KEY_KEYBOARD_2_AND_AT},
-    {"...--", HID_USAGE_KEY_KEYBOARD_3_AND_HASH},
-    {"....-", HID_USAGE_KEY_KEYBOARD_4_AND_DOLLAR},
-    {".....", HID_USAGE_KEY_KEYBOARD_5_AND_PERCENT},
-    {"-....",  HID_USAGE_KEY_KEYBOARD_6_AND_CARET},
-    {"--...", HID_USAGE_KEY_KEYBOARD_7_AND_AMPERSAND},
-    {"---..", HID_USAGE_KEY_KEYBOARD_7_AND_AMPERSAND},
-    {"----.", HID_USAGE_KEY_KEYBOARD_9_AND_LEFT_PARENTHESIS},
+    {".-", A},
+    {"-...", B},
+    {"-.-.", C},
+    {"-..", D},
+    {".", E},
+    {"..-.", F},
+    {"--.", G},
+    {"....", H},
+    {"..", I},
+    {".---", J},
+    {"-.-", K},
+    {".-..", L},
+    {"--", M},
+    {"-.", N},
+    {"---", O},
+    {".--.", P},
+    {"--.-", Q},
+    {".-.", R},
+    {"...", S},
+    {"-", T},
+    {"..-", U},
+    {"...-", V},
+    {".--", W},
+    {"-..-", X},
+    {"-.--", Y},
+    {"--..", Z},
+    {"-----", N0},
+    {".----", N1},
+    {"..---", N2},
+    {"...--", N3},
+    {"....-", N4},
+    {".....", N5},
+    {"-....",  N6},
+    {"--...", N7},
+    {"---..", N8},
+    {"----.", N9},
 }; 
 
+static void emit_key(uint32_t keycode) {
+    raise_k_keycode_state_changed_from_encode(keycode, true, state.last_event_timestamp);
+    raise_k_keycode_state_changed_from_encode(keycode, false, state.last_event_timestamp); 
+}
 
 static uint8_t lookup_morse(const char *buffer, uint8_t len) {
     for (size_t i = 0; i < sizeof(morse_table)/sizeof(morse_table[0]); i++) {
@@ -87,59 +93,61 @@ static uint8_t lookup_morse(const char *buffer, uint8_t len) {
             return morse_table[i].keycode;
         }
     }
-    return 0; // 0 means not found
+    return false;
 }
-
-static void flush_letter(struct k_work *work) {
+static void flush_letter_now(void) {
     if (state.count == 0) {
         return;
     }
-
     uint8_t keycode = lookup_morse(state.buffer, state.count);
     if (keycode) {
-        raise_k_keycode_state_changed_from_encode(keycode, true, event_timestamp); // put the proper usage of this code later on I guess, you know what to do, you're a smart cookie! Good luck!
-        zmk_hid_keyrelease(keycode); //todo this is wrong code, this doesn't exist in zmk
+        emit_key(keycode)
     } else {
         // Invalid sequence: output literal dots/dashes
         for (uint8_t i = 0; i < state.count; i++) {
             if (state.buffer[i] == '.') {
-                zmk_hid_keypress(HID_USAGE_KEY_KEYBOARD_PERIOD_AND_GREATER_THAN);
-                zmk_hid_keyrelease(HID_USAGE_KEY_KEYBOARD_PERIOD_AND_GREATER_THAN);
+                emit_key(KP_DOT)
             } else if (state.buffer[i] == '-') {
-                zmk_hid_keypress(HID_USAGE_KEY_KEYBOARD_MINUS_AND_UNDERSCORE);
-                zmk_hid_keyrelease(HID_USAGE_KEY_KEYBOARD_MINUS_AND_UNDERSCORE);
+                emit_key(KP_MINUS)
             }
         }
     }
-
     state.count = 0;
 }
 
+
+static void flush_letter(struct k_work *work) {
+    ARG_UNUSED(work)
+    flush_letter_now()
+}
+
 static void flush_word(struct k_work *work) {
+    ARG_UNUSED(work)
     if (state.count == 0) {
-        zmk_hid_keypress(HID_USAGE_KEY_KEYBOARD_SPACEBAR);
-        zmk_hid_keyrelease(HID_USAGE_KEY_KEYBOARD_SPACEBAR);
+        emit_key(SPACE)
     }
 }
 
-static void schedule_flush_letter(int64_t unit_ms) {
-    k_work_reschedule(&state.flush_letter_work, K_MSEC(unit_ms * 3)); //todo check k_work_reschedule
-    k_work_reschedule(&state.flush_word_work, K_MSEC(unit_ms * 7));
+static void schedule_flush() {
+    k_work_reschedule(&state.flush_letter_work, K_MSEC(MORSE_TIME_UNIT * 3));
+    k_work_reschedule(&state.flush_word_work, K_MSEC(MORSE_TIME_UNIT * 7));
 }
 
 static void on_morse_binding_pressed(struct zmk_behavior_binding *binding,
                                                  struct zmk_behavior_binding_event event) {
+    state.last_event_timestamp = event.timestamp;
+    state.press_start_time = event.timestamp:
     // Cancel pending flushes while key is pressed
     k_work_cancel_delayable(&state.flush_letter_work);
     k_work_cancel_delayable(&state.flush_word_work);
-
-    state.last_release_time = event.timestamp;
 }
 
 static void on_morse_binding_released(struct zmk_behavior_binding *binding,
                                                   struct zmk_behavior_binding_event event) {
-    const uint32_t unit = DT_INST_PROP(0, time_unit_ms); //todo WTF does this do?
-    int64_t duration = event.timestamp - state.last_release_time; 
+    state.last_event_timestamp = event.timestamp;
+    int64_t duration = event.timestamp - event.timestamp:
+    
+
 
     if (state.count < MAX_SYMBOLS) {
         if (duration < 2 * unit) {
@@ -149,10 +157,12 @@ static void on_morse_binding_released(struct zmk_behavior_binding *binding,
         }
     } else {
         // Overflow, flush immediately as invalid sequence
-        flush_letter(NULL); //todo why does the flush_letter have a unit_ms?
+        flush_letter_now();
+    	k_work_cancel_delayable(&state.flush_letter_work);
+    	k_work_cancel_delayable(&state.flush_word_work);
     }
-
-    schedule_flush_letter(unit);
+    state.last_event_timestamp = event.timestamp;
+    schedule_flush();
 }
 
 // API struct
