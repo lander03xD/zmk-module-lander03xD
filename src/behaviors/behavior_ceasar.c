36da99d0aa346c6c0390076632a84dcf98a080d9
Lander03xD <22810909+lander03xD@users.noreply.github.com>
2026-02-28 20:08:14 +0100

    added drovak transation

--- src/behaviors/behavior_ceasar.c
+++ src/behaviors/behavior_ceasar.c
@@ -26,19 +26,71 @@ struct ceasar_state {
     bool encryption_active;
     bool last_event_state;
 };
+struct layout_mapping {
+    uint32_t keycode_from;
+    uint32_t keycode_to;
+};
+
+static const struct layout_mapping DV_QW[] = {
+    {A, A},
+    {B, N},
+    {C, I},
+    {D, H},
+    {E, D},
+    {F, Y},
+    {G, U},
+    {H, J},
+    {I, G},
+    {J, C},
+    {K, V},
+    {L, P},
+    {M, M},
+    {N, L},
+    {O, S},
+    {P, R},
+    {Q, X},
+    {R, O},
+    {S, SEMICOLON}, 
+    {T, K},
+    {U, F},
+    {V, DOT}, 
+    {W, COMMA},   
+    {X, B},
+    {Y, T},
+    {Z, SLASH} 
+};
+
+static uint32_t dvorak_to_qwerty(uint32_t keycode){
+	for (i=0; i<26; i++){
+	    if (DV_QW[i].keycode_from = keycode){
+	        return DV_QW[i].keycode_to;
+	    }
+	}
+	return keycode;
+}
+
+static uint32_t qwerty_to_dvorak(uint32_t keycode){
+	for (i=0; i<26; i++){
+	    if (DV_QW[i].keycode_to = keycode){
+	        return DV_QW[i].keycode_from;
+	    }
+	}
+	return keycode;
+}
 
 static struct ceasar_state state = { 
 .encryption_active = false,
 .last_event_state = false };
 
 static uint32_t ceasar_transform(uint32_t keycode) {
-    uint32_t usage = keycode;// zmk_hid_get_usage_id(keycode);
+
+    uint32_t usage = dvorak_to_qwerty(keycode);
 
     if (usage >= HID_USAGE_KEY_KEYBOARD_A && usage <= HID_USAGE_KEY_KEYBOARD_Z) {
         uint32_t offset = usage - HID_USAGE_KEY_KEYBOARD_A;
         uint32_t rotated = (offset + ROT) % 26;
         uint32_t new_usage = HID_USAGE_KEY_KEYBOARD_A + rotated;
-        return new_usage;//zmk_hid_keycode_from_usage(new_usage);
+        return dvorak_to_qwerty(new_usage);
     }
 
     return keycode;
