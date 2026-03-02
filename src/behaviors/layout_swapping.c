ffdf8a6c28f81875f5f394b8e01488f2a3228b11
Lander03xD <22810909+lander03xD@users.noreply.github.com>
2026-03-02 20:37:30 +0100

    tried adding logging

--- src/behaviors/layout_swapping.c
+++ src/behaviors/layout_swapping.c
@@ -2,8 +2,8 @@
 #include <stdint.h>
 #include <dt-bindings/zmk/keys.h>
 
-//#include <zephyr/logging/log.h> //logging
-//LOG_MODULE_REGISTER(my_behavior, LOG_LEVEL_DBG); //logging
+#include <zephyr/logging/log.h> //logging
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL); //logging
 
 struct layout_mapping {
     uint32_t keycode_from;
@@ -42,7 +42,7 @@ static const struct layout_mapping DV_QW[] = {
 uint32_t dvorak_to_qwerty(uint32_t keycode){
     for (size_t i = 0; i < sizeof(DV_QW)/sizeof(DV_QW[0]); i++){
         if (DV_QW[i].keycode_from == keycode){
-//            LOG_DBG("Keycode mapped from: 0x%08" PRIx32, keycode);
+               LOG_DBG("LANDER: LOgging something");
 //            LOG_DBG("Keycode mapped to: 0x%08" PRIx32, DV_QW[i].keycode_to);
             return DV_QW[i].keycode_to;
         }
